598c53210251ef6fd8f05c646155208186db4e79
Mateus Santana <mateuss.dev@gmail.com>
2017-07-03 02:44:56 -0300

    update webpack.dist

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -1,26 +1,51 @@
 var webpack = require('webpack');
-var path    = require('path');
-var config  = require('./webpack.config');
+var path = require('path');
+var config = require('./webpack.config');
 
 config.output = {
   filename: '[name].bundle.js',
   publicPath: '',
-  path: path.resolve(__dirname, 'dist')
+  path: path.resolve(__dirname, 'dist'),
+  sourceMapFilename: '[name].map'
 };
 
 config.plugins = config.plugins.concat([
-
-  // Reduces bundles total size
+  // new webpack.LoaderOptionsPlugin({
+  //   minimize: true,
+  //   debug: false
+  // }),
   new webpack.optimize.UglifyJsPlugin({
+    beautify: false,
     mangle: {
-
-      // You can specify all variables that should not be mangled.
-      // For example if your vendor dependency doesn't use modules
-      // and relies on global variables. Most of angular modules relies on
-      // angular global variable, so we should keep it unchanged
+      screw_ie8: true,
+      keep_fnames: true,
       except: ['$super', '$', 'exports', 'require', 'angular']
-    }
+    },
+    compress: {
+      screw_ie8: true
+    },
+    comments: false,
+    // sourceMap: false,
+    // sourceMap: options.devtool && (options.devtool.indexOf("sourcemap") >= 0 || options.devtool.indexOf("source-map") >= 0),
   })
+  // Reduces bundles total size
+  // new webpack.optimize.UglifyJsPlugin({
+  //   beautify: false,
+
+  //   compress: {
+  //     screw_ie8: true
+  //   },
+  //   comments: false
+  //   mangle: {
+  //     screw_ie8: true,
+  //     keep_fnames: true,
+  //     // You can specify all variables that should not be mangled.
+  //     // For example if your vendor dependency doesn't use modules
+  //     // and relies on global variables. Most of angular modules relies on
+  //     // angular global variable, so we should keep it unchanged
+
+  //   }
+  // })
 ]);
 
 module.exports = config;
